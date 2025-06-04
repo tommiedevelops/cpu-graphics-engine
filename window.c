1749880a2789f633eea0aeda2f54d9c9106c2767
Thomas Dizon <thomas.dizon25@gmail.com>
2025-06-05 09:48:39 +1000

    currently working on displaying a single red pixel to the window. Only using GLFW for window and 2D texture to display content, everything else will do myself

--- window.c
+++ window.c
@@ -1,25 +1,42 @@
 #include <stdio.h>
 #include <GLFW/glfw3.h>
 
-#define SCREEN_WIDTH (800) // pixels
-#define SCREEN_HEIGHT (600)
+#define WIDTH (800) // pixels
+#define HEIGHT (600)
+#define WINDOW_NAME ("Renderer")
 
 int main(void) {
-	GLFWwindow* window;	
-	
-	printf("width={%d}, height={%d}\n", SCREEN_WIDTH, SCREEN_HEIGHT);
-	
-	
-	if(!glfwInit()) {return -1;}
-	window = glfwCreateWindow(640,480, "Hello World", NULL, NULL);
-	
-	if(!window) { glfwTerminate(); return -1;}
+	GLFWwindow* window;
+	uint32_t framebuffer[WIDTH*HEIGHT] = {0}; //buffer for what will be drawn to screen
 
+	printf("width={%d}, height={%d}\n", WIDTH, HEIGHT);
+
+	// Initialising GLFW window
+	if(!glfwInit()) {return -1;}
+	window = glfwCreateWindow(WIDTH,HEIGHT, WINDOW_NAME, NULL, NULL);
+	if(!window) { glfwTerminate(); return -1;}
 	glfwMakeContextCurrent(window);
 
+	//Drawing a single red pixel to the middle of the screen
+	int x = 200;
+	int y = 200;
+	framebuffer[y * WIDTH + x] = 0xFF0000FF;
+
+	// upload framebuffer to a texture
+	GLuint tex;
+	glGenTextures(1, &tex);
+	glBindTexture(GL_TEXTURE_2D, tex);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, WIDTH, HEIGHT, 0, GL_RGBA, GL_UNSIGNED_BYTE, framebuffer);
+
+
+	// Select color of background
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+
+	// Rendering loop
 	while(!glfwWindowShouldClose(window))
 	{
 		glClear(GL_COLOR_BUFFER_BIT);
+		glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, WIDTH, HEIGHT, GL_RGBA, GL_UNSIGNED_BYTE, framebuffer);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
