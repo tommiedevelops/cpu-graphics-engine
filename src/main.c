60384c223ec13ac1a7d69d180e3a2f1649476728
Thomas Dizon <thomas.dizon25@gmail.com>
2025-07-05 22:51:49 +1000

    started planning out scene, transform, entity structures

--- src/main.c
+++ src/main.c
@@ -26,22 +26,9 @@ int main(int argc, char* argv[]) {
 	/* Parse 3D Model from .obj file */
 	struct ObjData obj_data = parse_obj(filename);
 
-	/* Vertex Operations */
-
-	// Normalize vertices
-	float length_scale = 500.0f;
-	normalize_vertices(length_scale, obj_data.vertices, obj_data.num_vertices);
-
-        // Reflect vertices vertically
-        for(int i = 0; i < obj_data.num_vertices; i++){
-                obj_data.vertices[i].y = -1 * obj_data.vertices[i].y;
-        }
-
-        // Translate vertices so model is in screen center
-        for(int i = 0; i < obj_data.num_vertices; i++){
-                obj_data.vertices[i].x += (int)WIDTH/2;
-                obj_data.vertices[i].y += (int)HEIGHT/2;
-        }
+	// Prepare a Scene with a single Transform
+	// Create Mesh from obj_data and attach to Transform
+		
 
 	/* Initialize frame buffer */
 	uint32_t framebuffer[WIDTH * HEIGHT] = {0};
@@ -57,10 +44,30 @@ int main(int argc, char* argv[]) {
                 while (SDL_PollEvent(&event)) {
                         if(event.type == SDL_QUIT) running = false;
                 }
-		// Operate on vertices
-		// Apply a rotation matrix that rotates it about its y-axis
 		
-		// Render 3D model to framebuffer
+		// Let Scene evolve with 'Time?'
+		// - Maybe give the Transform an 'Angular Velocity?'
+		
+		// Extract vertices and triangles from the Scene in World Coordinates
+
+		// Operate on those vertices to prepare them to be displayed on the screen
+
+		// Normalize vertices
+		float length_scale = 500.0f;
+		normalize_vertices(length_scale, obj_data.vertices, obj_data.num_vertices);
+
+        	// Reflect vertices vertically
+        	for(int i = 0; i < obj_data.num_vertices; i++){
+                	obj_data.vertices[i].y = -1 * obj_data.vertices[i].y;
+        	}
+
+        	// Translate vertices so model is in screen center
+        	for(int i = 0; i < obj_data.num_vertices; i++){
+                	obj_data.vertices[i].x += (int)WIDTH/2;
+                	obj_data.vertices[i].y += (int)HEIGHT/2;
+        	}
+		
+		// Rasterize triangles in the scene to the framebuffer
 	        render_triangles(framebuffer, obj_data.triangles, obj_data.num_triangles);
 
 		// Update SDL2 window w/ new framebuffer
