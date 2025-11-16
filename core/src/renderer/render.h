977d006cd5e09a92556ff02bf4d0a21f0f666fb8
Thomas Dizon <thomas.dizon25@gmail.com>
2025-11-16 11:24:08 +1100

    fleshing out 'render pass' concept

--- core/src/renderer/render.h
+++ core/src/renderer/render.h
@@ -9,6 +9,16 @@ typedef struct Scene Scene;
 typedef struct FrameBuffer FrameBuffer;
 typedef struct Renderer Renderer;
 
+typedef struct RenderPass {
+	VSUniforms*  vs_u;
+	VertShaderF  vs;
+
+	FSUniforms*  fs_u;
+	FragShaderF  fs;
+	
+	FrameBuffer* fb;
+} RenderPass;
+
 typedef struct Pipeline {
 	VertShaderF vs;
 	FragShaderF fs;
@@ -28,3 +38,14 @@ void render_wireframe(FrameBuffer* fb, Triangle* triangles, int num_triangles);
 void renderer_draw_scene(Renderer* r, FrameBuffer* fb ,Scene* scene);
 
 #endif
+
+
+/*
+ *  A render pass should be something that the user can create themselves and add.
+ *  RenderPass* rp = RenderPass_Create(VertShaderF vs, FragShaderF fs, TargetBuffer target);
+ *  Renderer_AddRenderPass(RenderPass* rp);
+ *  
+ *  Then when Renderer_Draw_Scene() is called, it should loop through each render pass, fill in the Uniforms and execute 
+ *  the pass.
+ *
+ */
