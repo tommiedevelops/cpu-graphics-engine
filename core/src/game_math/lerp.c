d106c6e3c31088ec34102c8579bafc09dbdcf4aa
tommiedevelops <thomas.dizon25@gmail.com>
2026-01-14 17:26:13 +1100

    tiny change was made

--- core/src/game_math/lerp.c
+++ core/src/game_math/lerp.c
@@ -7,8 +7,6 @@ float lerp_float(float u, float v, float t){
 }
 
 Vec2f lerp_vec2f(Vec2f u, Vec2f v, float t){
-	if(t > 1.0f) t = 1.0f;
-	if(t < 0.0f) t = 0.0f;	
 	return vec2f_add(vec2f_scale(u, 1-t), vec2f_scale(v, t));
 }
 
