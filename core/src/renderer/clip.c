7913ff5133f68250dc29cfe72466fc63a2c673db
tommiedevelops <thomas.dizon25@gmail.com>
2026-01-26 14:30:18 +1100

    started adding documentation to core/src/renderer/clip.c

--- core/src/renderer/clip.c
+++ core/src/renderer/clip.c
@@ -1,3 +1,10 @@
+/// -------------------------------------------------------------
+/// Implements Sutherland-Hodgman clipping algorithm to clip
+/// vertices against the canonical view volume (Vulkan)
+///
+/// Assumes vertices are in homogeneous clip space (see render.c)
+/// -------------------------------------------------------------
+
 #include <string.h>
 #include <stdlib.h>
 #include "game_math/lerp.h"
@@ -6,14 +13,26 @@
 #include "renderer/triangle.h"
 #include "error_log.h"
 
-#define NUM_PLANES (6)
 #define CLIP_IN_SIZE (3)
 #define CLIP_OUT_SIZE (16)
 
-static void calculate_clipping_planes(struct Plane4* planes){
-	/* all normals facing 'plane4_inside'*/
-	/* plane4_inside => -w<=x<=w, -w<=y<=w, 0<=z<=w*/
+/// Initializes the six clipping planes of the canonical view volume
+/// using the Vulkan clip-space convention.
+///
+/// The planes are defined in homogeneous clip space such that a point
+/// is considered inside the frustum if:
+///
+///     -w ≤ x ≤ w
+///     -w ≤ y ≤ w
+///      0 ≤ z ≤ w
+///
+/// All plane normals are oriented so that plane4_inside(plane, v) returns
+/// true for vertices inside the view volume.
+///
+/// @param planes Output array of 6 Plane4 structures receiving the
+///               clip-space frustum planes. Must be preallocated.
 
+static void calculate_clipping_planes(Plane4* planes){
 	//top (y = w)
 	planes[0].n = vec4f_create(0.0f, -1.0f, 0.0f, 1.0f);
 	planes[0].p = vec4f_create(0.0f, 1.0f, 0.0f, 1.0f);
@@ -39,6 +58,22 @@ static void calculate_clipping_planes(struct Plane4* planes){
 	planes[5].p = vec4f_create(0.0f, 0.0f, 1.0f, 1.0f);
 }
 
+/// Computes the interpolated vertex attributes at a line-plane intersection
+///
+/// Given start and end vertices of an edge in clip space, this function
+/// linearly interpolates all per-vertex attributes using parameter t,
+/// where t ∈ [0, 1] represents the intersection point along the segment:
+///
+///  P(t) = S + t (E - S)
+///
+/// This is used during homogeneous clipping to generate new vertices that 
+/// lie exactly on the clipping plane
+///
+/// @param s Start vertex of the edge
+/// @param e End vertex of the edge
+/// @param i Output vertex receiving interpolated attributes (must not be NULL)
+/// @param t Interpolation parameter along the edge, computed from plane-line
+/// 	     intersection in homogeneous coordinates
 static void compute_intersection(VSout s, VSout e, VSout* i, float t) {
 
 	if(!i){
@@ -52,6 +87,7 @@ static void compute_intersection(VSout s, VSout e, VSout* i, float t) {
 	i->uv_over_w = lerp_vec2f(s.uv_over_w, e.uv_over_w, t);
 	i->w_inv = lerp_float(s.w_inv, e.w_inv, t);
 }
+
 
 static void clip_edge(VSout s, VSout e, Plane4 P, VSout* out, int* out_n) {
 
@@ -78,10 +114,11 @@ static void clip_edge(VSout s, VSout e, Plane4 P, VSout* out, int* out_n) {
 
 }
 
-static int clip_against_plane(const Plane4 P, 
-		              const VSout* in, 
-			      const int in_n, 
-			      VSout* out){
+/// Performs Sutherland-Hodgman clipping of a polygon against a single plane P
+static void clip_against_plane(const Plane4 P, 
+		               const VSout* in, const int in_n, 
+			       VSout* out, int* out_n_ptr) 
+{
 	int out_n = 0;
 
 	for(int v = 0; v < in_n; v++){
@@ -90,14 +127,23 @@ static int clip_against_plane(const Plane4 P,
 		clip_edge(s,e,P,out,&out_n);
 	}
 
-	return out_n;
+	*out_n_ptr = out_n;
 }
 
-void swap_ptrs(void** a, void** b) {
+static inline void swap_ptrs(void** a, void** b) {
 	void* temp = *a;
 	*a = *b;
 	*b = temp;
 }
+
+/// Performs Sutherland-Hodgman clipping of a polygon (a triangle in this case)
+/// against an array of Planes representing the Vulkan Canonical View Volume.
+///
+/// Uses a 'Ping-Pong Buffer' solution to avoid having to allocate new memory
+///
+/// @param  in  Input triangle, assumed CCW winding
+/// @param  out Pre-allocated memory for the clip output. Minimum size of 16.
+/// @return     The number of vertices the clip output has. 
 
 int clip(const VSout in[3], VSout* out) {
 
@@ -106,14 +152,16 @@ int clip(const VSout in[3], VSout* out) {
 		return -1;
 	}
 	
+	const int NUM_PLANES = 6;
 	Plane4 planes[NUM_PLANES];
 	calculate_clipping_planes(planes);
 
-	// run clipping algorithm using ping pong buffers
+	// Ping-Pong buffers
 	VSout bufA[CLIP_OUT_SIZE] = {0};
 	VSout bufB[CLIP_OUT_SIZE] = {0};
 	int sizeA = 3, sizeB = 0;
 
+	// Pointers to current input and output buffers & respective sizes
 	VSout* in_ptr = bufA;
 	VSout* out_ptr = bufB;
 	int* in_size_ptr = &sizeA;
@@ -122,28 +170,27 @@ int clip(const VSout in[3], VSout* out) {
 	VSout* final_out = bufB;
 	int* final_size = &sizeB;
 
-	// write initial input into one of the buffers
 	for(int i = 0; i < 3; i++) bufA[i] = in[i];
-
+	
 	for(int i = 0; i < NUM_PLANES; i++){
-		*out_size_ptr = 0;
-		*out_size_ptr = clip_against_plane(
-					planes[i], 
-					in_ptr, *in_size_ptr,
-			       		out_ptr);
+		clip_against_plane(planes[i], 
+				   in_ptr, *in_size_ptr,
+			       	   out_ptr, out_size_ptr);
+
 		swap_ptrs((void*)&in_ptr,(void*)&out_ptr);
 		swap_ptrs((void*)&in_size_ptr, (void*)&out_size_ptr);
+
+		// The final output is given by the buffer and size we just 
+		// outputted to 
+
 		final_out = (final_out == bufB) ? bufA : bufB;
 		final_size = (final_size == &sizeB) 
 			      ?  &sizeA : &sizeB;
 	}
 
-	// write the results into output.
-	for(int i = 0; i < *final_size; i++){
-		out[i] = final_out[i];
-	}
+	for(int i = 0; i < *final_size; i++) out[i] = final_out[i];
 
-	return *in_size_ptr;
+	return *final_size;
 }
 
 
